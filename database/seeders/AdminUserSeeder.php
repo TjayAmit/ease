fb3a2b522f06c0efc969537463c705993ca0ecb4
= <=>
2026-03-14 22:13:51 +0800

    feat: make default user admin for development and apply spatie permission

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\{
+    Permission,
+    Role
+};
+use App\Models\User;
+
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_permissions = [
+            'create role',
+            'read role',
+            'update role',
+            'delete role',
+            'create permission',
+            'read permission',
+            'update permission',
+            'delete permission',
+            'create user',
+            'read user',
+            'update user',
+            'delete user',
+        ];
+
+        foreach ($role_permissions as $role_permission) {
+            Permission::create([
+                'name' => $role_permission,
+                'guard_name' => 'web',
+            ]);
+        }
+
+        $admin_role = Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web',
+        ]);
+
+        $admin_role->givePermissionTo($role_permissions);
+
+        $admin_user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@ease.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $admin_user->assignRole($admin_role);
+    }
+}
